91afd9ca543e3721df5ef8d91d4479424ff1c769
Saul H <saul.huerta@gmail.com>
2026-05-07 20:38:53 -0600

    Update

--- ExampleHashSet.java
+++ ExampleHashSet.java
@@ -13,5 +13,29 @@ class ExampleHashSet {
 
         System.out.println(set); // prints [Bob, Alice, David]
         System.out.println(set.size()); // prints 3
+
+        // ************************************************
+        // Performance test for HashSet
+        // The time to add, find, and remove all elements from the HashSet remains
+        // constant, regardless of the size of the HashSet. This showcases the
+        // efficiency of HashSet operations.
+
+        HashSet<String> numbersSet = new HashSet<String>();
+
+        // Add elements to HashSet
+        for (int i = 0; i < 1000; i++) {
+            numbersSet.add("element_" + i);
+        }
+
+        // Find elements in HashSet
+        for (int i = 0; i < 1000; i++) {
+            numbersSet.contains("element_" + i);
+        }
+
+        // Remove elements from HashSet
+        for (int i = 0; i < 1000; i++) {
+            numbersSet.remove("element_" + i);
+        }
+
     }
 }
