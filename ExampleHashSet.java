fa6fc753630af4bbb9e0bf4335ad1cfe42c66271
Saul H <saul.huerta@gmail.com>
2026-05-07 21:46:09 -0600

    Upodate

--- ExampleHashSet.java
+++ ExampleHashSet.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 class ExampleHashSet {
@@ -37,5 +39,66 @@ class ExampleHashSet {
             numbersSet.remove("element_" + i);
         }
 
+        example1(); // Anagram Matcher
+        example2(); // Find First Duplicate ID
+
     }
+
+    // ================ Exercice 1 - Anagram Matcher ================
+    static void example1() {
+        String[] array1 = { "listen", "silent", "enlist", "inlets", "google" };
+        String[] array2 = { "silent", "enlist", "inlets", "google", "glooge" };
+
+        HashSet<String> sortedWordsInArray2 = new HashSet<>();
+        for (String word : array2) {
+            sortedWordsInArray2.add(sortCharacters(word));
+        }
+
+        HashSet<String> anagramsMatched = new HashSet<>();
+        ArrayList<String> result = new ArrayList<>();
+        for (String word : array1) {
+            if (sortedWordsInArray2.contains(sortCharacters(word))) {
+                if (!anagramsMatched.contains(word)) {
+                    result.add(word);
+                    anagramsMatched.add(word);
+                }
+            }
+        }
+
+        System.out.println(result); // prints [listen, silent, enlist, inlets]
+    }
+
+    private static String sortCharacters(String input) {
+        char[] chars = input.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    // ==============================================================
+
+    // ================ Exercice 1 - Anagram Matcher ================
+    static void example2() {
+        String[] ids1 = { "X123", "A456", "X123", "B789", "A456", "C111" };
+        String[] ids2 = { "Z999", "Y888", "Z999", "Y888" };
+        String[] ids3 = { "E100", "B200", "C300", "E100", "D400", "C300" };
+
+        System.out.println(findFirstDuplicateID(ids1)); // Expected "X123"
+        System.out.println(findFirstDuplicateID(ids2)); // Expected "Z999"
+        System.out.println(findFirstDuplicateID(ids3)); // Expected "E100"
+    }
+
+    public static String findFirstDuplicateID(String[] ids) {
+        HashSet<String> idSet = new HashSet<>();
+        for (String id : ids) {
+            if (!idSet.contains(id)) {
+                idSet.add(id);
+            } else {
+                return id;
+            }
+        }
+
+        // Return an empty string if no duplicate ids are found
+        return "";
+    }
+    // ==============================================================
 }
