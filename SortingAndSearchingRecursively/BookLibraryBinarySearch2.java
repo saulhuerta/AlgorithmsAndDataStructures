03294d1e203bf4a20cca385708ac98335aca242a
Saul H <saul.huerta@gmail.com>
2026-05-15 15:06:12 -0600

    Adding new files

--- SortingAndSearchingRecursively/BookLibraryBinarySearch2.java
+++ SortingAndSearchingRecursively/BookLibraryBinarySearch2.java
@@ -2,26 +2,31 @@ public class BookLibraryBinarySearch2 {
 
     static int binarySearchForBook(int[] bookIDs, int start, int end, int searchID) {
         System.out.println("Book IDs: " + bookIDs + " - Start: " + start + " - End: " + end + " SearchID: " + searchID);
-        //if (start > end) return -1; // Base case
-        //if (start > end) return (end + 1) * -1; // Base case
-        if (start > end) return start * -1; // Base case
+
+        if (start > end)
+            return start * -1; // Base case
+
         int mid = start + (end - start) / 2; // Find the midpoint
-        if (bookIDs[mid] == searchID) return mid; // Target found
+
+        if (bookIDs[mid] == searchID)
+            return mid; // Target found
+
         if (bookIDs[mid] > searchID) // If the target is less than the midpoint
             return binarySearchForBook(bookIDs, start, mid - 1, searchID); // Search the left half
+
         return binarySearchForBook(bookIDs, mid + 1, end, searchID); // Search the right half
     }
 
     public static void main(String[] args) {
-       
-        int[] bookIDs = {101, 203, 307, 404, 513, 628, 734, 850, 961};
+
+        int[] bookIDs = { 101, 203, 307, 404, 513, 628, 734, 850, 961 };
         int searchID = 367;
         int bookIndex = binarySearchForBook(bookIDs, 0, bookIDs.length - 1, searchID);
 
         if (bookIndex >= 0)
             System.out.println("Book with ID " + searchID + " is at index: " + bookIndex);
         else
-            System.out.println("Book should be inserted in ID " + bookIndex * -1 );
+            System.out.println("Book should be inserted in ID " + bookIndex * -1);
     }
 
 }
